c860e734fec63ab63cfb2c054069837a544f14d7
Daniel <dasave85@gmail.com>
2022-05-09 12:57:02 +0200

    nivel 3,

--- ejercicio1.js
+++ ejercicio1.js
@@ -20,4 +20,16 @@ const returnValue = (value) => value
 
 console.log(`Este es el ${returnValue('valor a retornar')}`)
 
+// nivel 3, ejercicio 1
 
+const arrayFunctions = new Array(10)
+
+function printNumbers(){
+    for (let i = 0; i < 10; i++){
+        console.log(i)
+    }
+}
+
+for (let i = 0; i < arrayFunctions.length; i++){
+    arrayFunctions.push(printNumbers())
+}
