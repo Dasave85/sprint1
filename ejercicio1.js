3548c178c873decbbda771efab6914363f81c5df
Daniel <dasave85@gmail.com>
2022-05-09 12:43:46 +0200

    nivel 2, ej.1 y ej.2

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,7 +1,23 @@
-// nivel 1
+// nivel 1, ejercicio 1
 
 function printName(name) {
     console.log(name)
 }
 
 printName('Dani')
+
+// nivel 2, ejercicio 1 
+
+const name = 'Dani'
+const surname = 'Sanchez'
+
+console.log(`Nombre: ${name}
+Apellido: ${surname}`)
+
+// nivel 2, ejercicio 2
+
+const returnValue = (value) => value
+
+console.log(`Este es el ${returnValue('valor a retornar')}`)
+
+
