7cbb5417e482d4fa3816b1495a90833c5c9a77d9
Daniel <dasave85@gmail.com>
2022-05-09 18:21:43 +0200

    ejercicio 1 completo

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,35 +1,46 @@
 // nivel 1, ejercicio 1
 
 function printName(name) {
-    console.log(name)
+	console.log(name);
 }
 
-printName('Dani')
+printName('Dani');
 
-// nivel 2, ejercicio 1 
+// nivel 2, ejercicio 1
 
-const name = 'Dani'
-const surname = 'Sanchez'
+const name = 'Dani';
+const surname = 'Sanchez';
+const secondSurname = 'Vela';
 
 console.log(`Nombre: ${name}
-Apellido: ${surname}`)
+Apellidos: ${surname} ${secondSurname}`);
 
 // nivel 2, ejercicio 2
 
-const returnValue = (value) => value
+const returnValue = (value) => value;
 
-console.log(`Este es el ${returnValue('valor a retornar')}`)
+console.log(`Este es el ${returnValue('valor a retornar')}`);
 
 // nivel 3, ejercicio 1
 
-const arrayFunctions = new Array(10)
+const arrayFunctions = [];
 
-function printNumbers(){
-    for (let i = 0; i < 10; i++){
-        console.log(i)
-    }
+function printNumbers() {
+	for (let i = 0; i < 10; i++) {
+		console.log(i);
+	}
 }
 
-for (let i = 0; i < arrayFunctions.length; i++){
-    arrayFunctions.push(printNumbers())
+for (let i = 0; i < 10; i++) {
+	arrayFunctions.push(printNumbers);
 }
+
+arrayFunctions.forEach((element) => {
+	element();
+});
+
+// nivel 3, ejercicio 2
+
+const nameUser = (function(nameUser) {
+	console.log(nameUser);
+})('Daniel Sanchez');
