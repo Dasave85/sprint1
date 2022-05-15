0be0b8dc236b63303ef0fca08e11acac910f0086
Daniel <dasave85@gmail.com>
2022-05-15 17:42:20 +0200

    exercise 1.4 complete

---
+++ Entrega_1.4_Async_Await.js
@@ -0,0 +1,116 @@
+//nivel 1, ejercicio 1
+
+let employees = [
+	{
+		id: 1,
+		name: 'Linux Torvalds'
+	},
+	{
+		id: 2,
+		name: 'Bill Gates'
+	},
+	{
+		id: 3,
+		name: 'Jeff Bezos'
+	}
+];
+
+let salaries = [
+	{
+		id: 1,
+		salary: 4000
+	},
+	{
+		id: 2,
+		salary: 1000
+	},
+	{
+		id: 3
+		// salary: 2000
+	}
+];
+
+const getEmployee = (num) => {
+	return new Promise((resolve, reject) => {
+		let condition;
+		for (let employe of employees) {
+			if (employe.id === num) {
+				condition = employe;
+			}
+		}
+		if (condition) {
+			resolve(condition);
+		} else {
+			reject(new Error('el empleado no existe'));
+		}
+	});
+};
+
+const getSalary = (employe) => {
+	return new Promise((resolve, reject) => {
+		let salaryEmploye;
+		for (let salary of salaries) {
+			if (salary.id === employe.id) {
+				salaryEmploye = salary.salary;
+			}
+		}
+		if (salaryEmploye) {
+			resolve(salaryEmploye);
+		} else {
+			reject(new Error('error'));
+		}
+	});
+};
+
+async function employeeData(id) {
+	try {
+		const employee = await getEmployee(id);
+		const salary = await getSalary(employee);
+		console.log(`El empleado ${employee.name}, recibe ${salary} € mensuales`);
+	} catch (err) {
+		console.log(err);
+	}
+}
+
+employeeData(2);
+
+//nivel 1, ejercicio 2
+
+function getDate() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			const date = new Date();
+			resolve(console.log(date.toDateString()));
+		}, 2000);
+	});
+}
+
+async function date() {
+	await getDate();
+}
+
+date();
+
+//nivel 2, ejercicio 1
+
+const double = (num) => {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve(num * 2);
+		}, 2000);
+	});
+};
+
+async function addDoubles(num1, num2, num3) {
+	const doubleNum1 = await double(num1);
+	const doubleNum2 = await double(num2);
+	const doubleNum3 = await double(num3);
+	console.log(doubleNum1 + doubleNum2 + doubleNum3);
+}
+
+addDoubles(1, 2, 3);
+
+//nivel 3,  ejercicio 1
+
+getEmployee(4).catch((err) => console.log(err));
+getSalary(3).catch((err) => console.log(err));
